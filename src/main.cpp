820debd527219fb7116e91953525e582d2bbcefb
Bimi <dim.ziakas07@gmail.com>
2023-01-05 18:39:42 +0200

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,70 @@
 #include <Arduino.h>
 #include <stdint.h>
+#include <SPI.h>
 #include <Adafruit_BME680.h>
 #include <Adafruit_BNO055.h>
 #include <Adafruit_GPS.h>
+#include <RH_RF95.h>
 #include <Pixy2.h>
+#include <SD.h>
+
 #include "detection.h"
+#include "RF.h"
+
+#define FREQUENCY 434.2
+#define PRESSURE 1013.25 // to change if needed
 
 Adafruit_BME680 BME680 = Adafruit_BME680();
 Adafruit_BNO055 BNO055 = Adafruit_BNO055();
 Adafruit_GPS GPS = Adafruit_GPS();
+RH_RF95 RF = RH_RF95();
 Pixy2 Cam = Pixy2();
 
+File dataFile;
 void setup() {
     Serial.begin(115200);
 
     Cam.init();
     Cam.changeProg("video");
 
-    bool BME_BOOT = BME680.begin();
-    bool BNO_BOOT = BNO055.begin();
+    BME680.begin();
+    BNO055.begin();
 
-    const char* BME_INIT = "[Debug] BME680: Initialised!";
-    const char* BNO_INIT = "[Debug] BNO055: Initialised!";
+    RF.init();
+    RF.setFrequency(FREQUENCY);
 
-    if (!BME_BOOT) {
-        BME_INIT = "[Debug] BME680: Failed to initialise.";
+    if (SD.begin()) {
+        SD.remove("data.txt"); // if it doesn't exist, nothing wrong happens :D
+        dataFile = SD.open("data.txt", FILE_WRITE);
     }
-
-    if (!BNO_BOOT) {
-        BNO_INIT = "[Debug] BNO055: Failed to initialise.";
-    }
-
-    Serial.println(BME_INIT);
-    Serial.println(BNO_INIT);
 }
 
+
+
 void loop() {
-    uint8_t phase = detectPhase(BME680);
+    uint8_t phase = detectPhase(BME680.readAltitude(PRESSURE));
 
     float temperature = BME680.readTemperature();
     float pressure = BME680.readPressure();
+    float humidity = BME680.readHumidity();
+
+    //? find a way around gps
     char position = GPS.read();
 
+    // TODO: add gps-related data
+    if (dataFile) {
+        // allocate memory worth 100 string characters for data
+        char* data = (char*) malloc(100 * sizeof(char));
+        if (data != NULL) {
+            memset(data, 0, 100 * sizeof(char));
+            snprintf(data, 100 * sizeof(char), "%.02f %.02f %.02f", temperature, pressure, humidity);
+            dataFile.println(data);
+            dataFile.flush();
+            RFsend(&RF);
+            free(data);
+        }
+
+        // if allocation failed, data is NULL
+        Serial.println("[Debug]: Failed to allocate enough memory for the mission data.");
+    }
 }
