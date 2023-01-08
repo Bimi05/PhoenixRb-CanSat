81b65c750ece80d56809c04e70e9ef81a16e2894
Bimi <dim.ziakas07@gmail.com>
2023-01-08 22:51:21 +0200

    update RF files

--- include/RF.h
+++ include/RF.h
@@ -1,4 +1,10 @@
 #include <stdint.h>
+#include <Arduino.h>
 #include <RH_RF95.h>
 
-void RFsend(RH_RF95 *RF);
+class RFM96W {
+    public:
+        RFM96W(float freq = 0.0);
+        void init();
+        void send(const char *data);
+};
--- src/RF.cpp
+++ src/RF.cpp
@@ -1,7 +1,30 @@
 #include <stdint.h>
+#include <Arduino.h>
 #include <RH_RF95.h>
 #include "RF.h"
 
-void RFsend(RH_RF95 *RF) {
+class RFM96W {
+    private:
+        float frequency;
+        RH_RF95 RF = RH_RF95();
 
-}
+    public:
+        RFM96W(float freq = 0.0) {
+            frequency = freq;
+        }
+
+        void init() {
+            if (!RF.init()) {
+                Serial.println("[Debug]: RFM96W: Could not initialise.");
+            }
+
+            if (frequency != 0.0) {
+                RF.setFrequency(frequency);
+            }
+            // TODO: add whatever else is needed for initialisation
+        }
+
+        void send(const char *data) {
+            // TODO: send the mission data here
+        }
+};
