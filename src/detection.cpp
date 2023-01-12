e924c618589e06013c81b956fe7129cf635c16ba
Bimi <dim.ziakas07@gmail.com>
2023-01-13 00:32:45 +0200

    improve code comments

--- src/detection.cpp
+++ src/detection.cpp
@@ -5,9 +5,17 @@ float lastMeasure = 0.0F;
 
 uint8_t phase = 0;
 uint8_t detectPhase(float altitude) {
+    /*
+    * ----- PHASES ----- *
+    * Phase 1: before launch
+    * Phase 2: going up (inside the rocket)
+    * Phase 3: going down (free fall)
+    * Phase 4: after landing
+    * ------------------ *
+    */
+
     if (altitude != altitude) {
-        //* this returns true if altitude is NaN
-        return 0;
+        return 0; //! altitude is NaN
     }
 
     if (lastMeasure == 0.0) {
@@ -15,17 +23,11 @@ uint8_t detectPhase(float altitude) {
         return 0;
     }
 
-    // TODO: improve and perfect after testing
     float diff = altitude - lastMeasure;
     if (diff >= 0.0F && diff < 3.0F) {
-        // altitude hasn't changed much/at all, so we're on the ground
-        // phase 1 is before launch
-        // phase 4 is after landing
         phase = (phase != 3) ? 1 : 4;
     }
     else {
-        // if altitude increases (current > last) then we're in the rocket going up (phase 2)
-        // if altitude decreases (current < last) then we're falling from where the rocket launched us (phase 3)
         phase = (altitude > lastMeasure) ? 2 : 3;
     }
 
