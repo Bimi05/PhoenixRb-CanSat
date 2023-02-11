e83cd3badd6b2ba4d3e77c7cadd8602d5cdae7ab
Bimi05 <dim.ziakas07@gmail.com>
2023-02-11 19:16:18 +0200

    deleted RFM files, renamed phase detection files

--- include/detection.h
+++
@@ -1,3 +0,0 @@
-#include <stdint.h>
-
-uint8_t detectPhase(float);
---
+++ src/phases.cpp
@@ -0,0 +1,23 @@
+#include "phases.h"
+
+uint8_t phase = 1;
+bool landed = false;
+float measurings[5] = { 0.0F };
+
+uint8_t detectPhase(Adafruit_BME680& BME, float pressure) {
+    if (landed == true) {
+        return 4;
+    }
+
+    for (int i=0; i<5; i++) {
+        measurings[i] = BME.readAltitude(pressure);
+    }
+
+    //* check measures
+    //! ascending order => phase 2
+    //! descending order => phase 3
+    //! ascending then descending or just descending => phase 3
+    //! descending then stabilising => phase 4 --- set landed to true
+
+    return phase;
+}
