9bc5c023fbd9200903bc9144727c19842fac2455
Bimi05 <dim.ziakas07@gmail.com>
2023-02-11 19:17:12 +0200

    add `poi.h` and `poi.cpp` for POI-related code

---
+++ include/poi.h
@@ -0,0 +1,8 @@
+#include <Adafruit_BNO055.h>
+#include <Pixy2I2C.h>
+#include <Servo.h>
+
+#include <stdint.h>
+
+char* findPOI(Pixy2I2C& Cam);
+void moveToPOI(Adafruit_BNO055& BNO, Servo& Servo1, Servo& Servo2, const char *);
---
+++ src/poi.cpp
@@ -0,0 +1,16 @@
+#include "poi.h"
+
+char* findPOI(Pixy2I2C& Cam) {
+    char *position = (char*) malloc(10 * sizeof(char));
+
+    uint8_t x = 120;
+    uint8_t y = 60;
+    // TODO: find POI through Pixy2 camera
+
+    snprintf(position, 10, "%i,%i", x, y);
+    return position;
+}
+
+void moveToPOI(Adafruit_BNO055& BNO, Servo& Servo1, Servo& Servo2, const char *coordinates) {
+    // TODO: use BNO055 and both servos to move to the POI
+}
