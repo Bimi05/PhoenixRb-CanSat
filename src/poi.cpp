e8c8f67379f680388638ae04f866ac1e53d1363d
Bimi05 <dim.ziakas07@gmail.com>
2023-04-05 17:40:35 +0300

    readjustments and first piece of code for steering

--- src/poi.cpp
+++ src/poi.cpp
@@ -3,14 +3,21 @@
 char* findPOI(Pixy2I2C* Cam) {
     char *position = (char*) malloc(10 * sizeof(char));
 
-    uint8_t x = 120;
-    uint8_t y = 60;
+    float x = 0.0F;
+    float y = 0.0F;
     // TODO: find POI through Pixy2 camera
 
-    snprintf(position, 10, "%i,%i", x, y);
+    snprintf(position, 10, "%f,%f", x, y); //! x and y are RELATIVE to Pixy2's (0, 0)
     return position;
 }
 
 void moveToPOI(Adafruit_BNO055* BNO, Servo* ServoMotor, const char *coordinates) {
-    // TODO: use BNO055 and both servos to move to the POI
+    sensors_event_t data;
+    BNO->getEvent(&data);
+
+    float x = 0;
+    float y = 0;
+
+    float angle = atanf(x/y);
+    ServoMotor->write(angle / 6);
 }
