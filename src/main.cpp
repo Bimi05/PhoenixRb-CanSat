4d8f3a3926555f00ae52fe4073b5db1819bede53
Bimi05 <dim.ziakas07@gmail.com>
2023-04-18 00:34:38 +0300

    many changes, can't list them all

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
+
 #include <Adafruit_BME680.h>
 #include <Adafruit_BNO055.h>
 #include <Adafruit_GPS.h>
 #include <RH_RF95.h>
 #include <Pixy2I2C.h>
 #include <Servo.h>
-#include <SPI.h>
+
 #include <SD.h>
+#include <SPI.h>
+#include <TeensyThreads.h>
 
 #include "poi.h"
 #include "phases.h"
-
-#define SEALEVEL_PRESSURE 1013.25F //* used for altitude calculation (BME680) - change if necessary
 
 #define RFM_CS 10
 #define RFM_INT 8
@@ -20,140 +21,187 @@
 
 #define BUZ_PIN 22
 
-#define GPSSerial Serial1
-
 Adafruit_BME680 BME680 = Adafruit_BME680();
 Adafruit_BNO055 BNO055 = Adafruit_BNO055();
-Adafruit_GPS GPS = Adafruit_GPS(&GPSSerial);
+Adafruit_GPS GPS = Adafruit_GPS(&Serial1);
 RH_RF95 RFM = RH_RF95(RFM_CS, RFM_INT);
 Pixy2I2C Cam = Pixy2I2C();
-Servo ServoMotor = Servo(); //* servo motor for our controlled movement
+Servo ServoMotor = Servo();
 File dataFile;
 
-uint32_t ID = 1; //* packet ID; will be used by data analysis
+//? this is really just a shortcut to prevent repetitions
+void play(uint8_t pin, uint16_t freq, uint32_t duration) {
+    tone(pin, freq);
+    delay(duration);
+    noTone(pin);
+}
+
+uint32_t initTime = 0;
 char *data = (char*) malloc(255 * sizeof(char)); //* a message of maximum 255 characters (RFM limit)
+float ground_pressure = static_cast<float>(NAN); //! we will calculate this ourselves
+
+void mainMission(void) {
+    static uint32_t ID = 1; //* packet ID; will be used by data analysis
+    while (true) {
+        BME680.beginReading();
+
+        float temp = static_cast<float>(NAN);
+        float pres = static_cast<float>(NAN);
+        float hum = static_cast<float>(NAN);
+        float alt = static_cast<float>(NAN);
+        float lat = static_cast<float>(NAN);
+        float lon = static_cast<float>(NAN);
+
+
+        bool parsedNMEAs = false;
+        for (uint8_t i=0; i<5; i++) {
+            do {
+                GPS.read();
+            } while (!GPS.newNMEAreceived());
+
+            if (i > 2) {
+                //* read last 2 NMEA sentences, to avoid possible corruption
+                //* if at least one is parsed, the boolean will be set to true
+                parsedNMEAs = GPS.parse(GPS.lastNMEA());
+            }
+        }
+
+        if (parsedNMEAs) {
+            lat = (GPS.lat == 'S') ? -(GPS.latitude) : GPS.latitude;
+            lon = (GPS.lon == 'W') ? -(GPS.longitude) : GPS.longitude;
+        }
+
+        float time = static_cast<float>((millis() - initTime) / 1000.0F);
+
+        if (BME680.endReading()) {
+            temp = BME680.temperature;
+            pres = BME680.pressure / 100.0F;
+            hum = BME680.humidity;
+            alt = BME680.readAltitude(ground_pressure);
+        }
+
+        uint8_t len = snprintf(data, 255 * sizeof(char), "PRb:%li %.01f %.02f %.02f %.02f %.02f %.04f %.04f", ID, time, temp, pres, hum, alt, lat, lon);
+        RFM.send((uint8_t*) data, len);
+
+        if (dataFile) {
+            dataFile.println(data);
+            dataFile.flush();
+        }
+
+        ID++;
+    }
+}
 
 void setup(void) {
-    while (!Serial);
-    Serial.begin(115200);
-    Serial.println("[Debug] Beginning initialisation...");
-
     if (!BME680.begin()) {
-        Serial.println("[Debug] Could not initialise the BME680 Sensor.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
-        Serial.println("[Debug] Sensor BME680 initialised!");
+        for (int i=0; i<5; i++) {
+            ground_pressure += (BME680.readPressure() / 100.0F);
+        }
+        ground_pressure /= 5;
+
+        play(BUZ_PIN, 4000, 500);
+        delay(1000);
     }
+
 
     if (!BNO055.begin()) {
-        Serial.println("[Debug] Could not initialise the BNO055 Sensor.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
-        Serial.println("[Debug] Sensor BNO055 initialised!");
+        play(BUZ_PIN, 4000, 500);
+        delay(1000);
     }
 
+
     if (!GPS.begin(9600)) {
-        Serial.println("[Debug] Could not initialise the Ultimate GPS Sensor.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
         GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
         GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
-        Serial.println("[Debug] Sensor Ultimate GPS initialised!");
+        play(BUZ_PIN, 4000, 500);
+        delay(1000);
     }
 
+
     if (!SD.begin(23)) {
-        Serial.println("[Debug] Could not initialise the SD Card.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
-        dataFile = SD.open("data.txt", O_RDWR);
+        if (SD.exists("data.txt")) {
+            SD.remove("data.txt");
+        }
+
+        dataFile = SD.open("data.txt", FILE_WRITE);
         if (dataFile) {
-            dataFile.seek(0); //* start writing at first file byte, to overwrite everything
-            Serial.println("[Debug] SD card initialised!");
+            play(BUZ_PIN, 4000, 500);
+            delay(1000);
         }
     }
 
+
+    pinMode(RFM_RST, OUTPUT);
+    digitalWrite(RFM_RST, LOW);
+    delay(100);
+    digitalWrite(RFM_RST, HIGH);
+    delay(100);
+
     if (!RFM.init()) {
-        Serial.println("[Debug] Could not initialise the RFM9x LoRa Sensor.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
         RFM.setFrequency(RFM_FREQUENCY);
         RFM.setModeTx();
         RFM.setTxPower(20); //* max power for possibly better transmissions
-        Serial.println("[Debug] Sensor RFM9x LoRa initialised!");
+        play(BUZ_PIN, 4000, 500);
+        delay(1000);
     }
 
+
     if (Cam.init() != 0) {
-        Serial.println("[Debug] Could not initialise Pixy2.");
+        play(BUZ_PIN, 3000, 500);
+        delay(1000);
     }
     else {
-        Cam.changeProg("video");
-        Serial.println("[Debug] Camera Pixy2 initialised!");
+        play(BUZ_PIN, 4000, 500);
+        delay(1000);
     }
 
     ServoMotor.attach(3);
 
-    Serial.println("[Debug] Initialisation complete!");
-    Serial.println("----------------------------------------------------------------------");
+    initTime = millis();
+    threads.addThread(mainMission);
+
+
+    play(BUZ_PIN, 3322, 500);
+    delay(1000);
+    play(BUZ_PIN, 2489, 500);
+    delay(1000);
+    play(BUZ_PIN, 1661, 500);
+    delay(1000);
+    play(BUZ_PIN, 1864, 500);
+    delay(1000);
 }
 
-#define DEBUG_MODE //! remove/comment out when done testing
-
 void loop(void) {
-    uint32_t start = millis();
-    memset(data, '-', 255 * sizeof(char));
-
-    BME680.performReading();
-
-    float temp = BME680.temperature;
-    float pres = BME680.pressure / 100.0F;
-    float hum = BME680.humidity;
-    float alt = BME680.readAltitude(SEALEVEL_PRESSURE);
-
-    for (uint8_t i=0; i<2; i++) {
-        while (!GPS.newNMEAreceived()) {
-            GPS.read();
-        }
-    }
-    GPS.parse(GPS.lastNMEA());
-
-    float lat = (GPS.lat == 'S') ? -(GPS.latitude) : GPS.latitude;
-    float lon = (GPS.lon == 'W') ? -(GPS.longitude) : GPS.longitude;
-
-    float time = static_cast<float>(millis() / 1000.0F);
-    uint8_t len = snprintf(data, 255 * sizeof(char), "PRb:%li %.01f %.02f %.02f %.02f %.02f %.04f %.04f", ID, time, temp, pres, hum, alt, lat, lon);
-
-
-    RFM.send((uint8_t*) data, len);
-
-    dataFile.println(data);
-    dataFile.flush();
-
-    #ifdef DEBUG_MODE
-        Serial.println(data);
-    #endif
-
-    // uint8_t phase = detectPhase(&BME680, SEALEVEL_PRESSURE);
-    uint8_t phase = 1;
-    if (phase == 3) {
-        char* pos = findPOI(&Cam);
-        moveToPOI(&BNO055, &ServoMotor, pos);
+    uint8_t phase = detectPhase(&BME680, ground_pressure);
+    if (phase == 3 && (BME680.readAltitude(ground_pressure) < 800.0F)) {
+        findPOI(&Cam);
+        move(&ServoMotor);
     }
     else if (phase == 4) {
         static uint32_t last_beat = millis();
         if ((millis() - last_beat) > 2000) {
             last_beat = millis();
-
-            //! melody to be determined :)
-            tone(BUZ_PIN, 3322.438F, 500);
-            tone(BUZ_PIN, 2489.016F, 500);
-            tone(BUZ_PIN, 1661.219F, 500);
-            tone(BUZ_PIN, 1864.655F, 500);
+            play(BUZ_PIN, 4000, 500);
         }
-    }
-
-
-    ID++;
-    if ((millis() - start) < 100) {
-        delay(100 - (millis() - start));
     }
 }
