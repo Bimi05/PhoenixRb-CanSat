c979fa64ec394287be7aff1d6b82dc277e557d0a
Bimi05 <dim.ziakas07@gmail.com>
2023-04-18 10:56:18 +0300

    function definitions, and new data

--- src/poi.cpp
+++ src/poi.cpp
@@ -1,7 +1,21 @@
 #include "poi.h"
 
 uint8_t avg_red, avg_green, avg_blue;
-uint16_t position[2];
+
+uint8_t poi_index = 0;
+uint16_t desired_POI[2] = { 0, 0 };
+uint16_t POIs[5][2] = {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 }
+};
+
+bool scanned = false;
+bool sendPOI = false;
+char* pos = (char*) malloc(25 * sizeof(char));
+
 
 void findAverageValues(Pixy2I2C* Cam) {
     uint8_t r, g, b;
@@ -21,7 +35,6 @@ void findAverageValues(Pixy2I2C* Cam) {
     avg_blue /= (Cam->frameHeight * Cam->frameWidth);
 }
 
-bool scanned = false;
 
 void findPOI(Pixy2I2C* Cam) {
     if (scanned) {
@@ -30,33 +43,89 @@ void findPOI(Pixy2I2C* Cam) {
 
     findAverageValues(Cam);
 
+    uint8_t num = 0;
     uint8_t r, g, b;
-
     for (uint16_t i=0; i<Cam->frameHeight; i++) {
         for (uint16_t j=0; i<Cam->frameWidth; i++) {
             Cam->video.getRGB(j, i, &r, &g, &b);
+
+            if (abs(r-avg_red) > 50 && abs(g-avg_green) > 50 && abs(b-avg_blue) > 50) {
+                POIs[num][0] = (j-Cam->frameWidth);
+                POIs[num][1] = (i-Cam->frameHeight);
+                num++;
+            }
         }
     }
 
     scanned = true;
 }
 
-void move(Servo* ServoMotor) {
-    float x = static_cast<float>(position[0]);
-    float y = static_cast<float>(position[1]);
-    float angle = atanf(y/x); //* arctan(θ) gives us the angle to turn
 
-    if (angle != 0) {
-        int8_t min_value = (angle > 0) ? 5 : -5;
-        if (abs(angle) < abs(min_value)) {
-            min_value = angle;
-        }
+void setDesiredPOI(uint8_t num) {
+    desired_POI[0] = POIs[num][0];
+    desired_POI[1] = POIs[num][1];
+}
 
-        int8_t degrees = (abs(angle) > abs(min_value)) ? (angle/5) : min_value;
-        ServoMotor->write(((angle > 0) ? degrees : -(degrees)));
+
+void sendPosition(Adafruit_GPS* GPS, RH_RF95* RFM) {
+    if (!sendPOI) {
+        return;
+    }
+
+    memset(pos, 0, 50 * sizeof(char));
+    do {
+        GPS->read();
+    } while (!GPS->newNMEAreceived());
+    GPS->parse(GPS->lastNMEA());
+
+    float lat = (GPS->lat == 'S') ? -(GPS->latitude) : GPS->latitude;
+    float lon = (GPS->lon == 'W') ? -(GPS->longitude) : GPS->longitude;
+
+    uint8_t len = snprintf(pos, 25 * sizeof(char), "PoI:%.04f,%.04f", lat, lon);
+    RFM->send((uint8_t*) pos, len);
+    RFM->waitPacketSent();
+    sendPOI = false;
+}
+
+
+void move(Adafruit_BNO055* BNO, Servo* ServoMotor, bool force) {
+    //* force here is used for the PoI received by the ground station
+    //* the PoI locations will be sent to ground station one by one
+    //* the ground station will then pick one, send us the info
+    //* and we'll "force" land there
+
+    sensors_event_t BNO_data;
+    float x, y;
+
+    if (force) {
+        x = static_cast<float>(desired_POI[0]);
+        y = static_cast<float>(desired_POI[1]);
     }
     else {
-        //! we're gonna spin in circles until we land
-        ServoMotor->write(180);
+        x = static_cast<float>(POIs[poi_index][0]);
+        y = static_cast<float>(POIs[poi_index][1]);
     }
+
+    float angle = atanf(y/x); //* arctan(θ) gives us the angle to turn
+    float turn = 180.0F;
+
+    BNO->getEvent(&BNO_data);
+    if ((angle > -1.0F) && (angle < 1.0F)) {
+        if ((x > -1.0F) && (x < 1.0F)) {
+            if (!force) {
+                poi_index++;
+                sendPOI = true;
+            }
+        }
+        return;
+    }
+    else {
+        turn = (BNO_data.orientation.z + angle);
+    }
+
+    ServoMotor->write((angle > 0) ? 180 : 0);
+    while (abs(turn-BNO_data.orientation.z) > 1.0F) {
+        BNO->getEvent(&BNO_data);
+    }
+    ServoMotor->write(90);
 }
