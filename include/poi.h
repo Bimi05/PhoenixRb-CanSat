62cbc2d24c00fd1f5d2838fb5ce8af7e09a21321
Bimi05 <dim.ziakas07@gmail.com>
2023-04-18 10:55:50 +0300

    two new functions, parameter and include changes

--- include/poi.h
+++ include/poi.h
@@ -1,6 +1,12 @@
+#include <Adafruit_BNO055.h>
+#include <Adafruit_GPS.h>
+#include <RH_RF95.h>
 #include <Pixy2I2C.h>
 #include <Servo.h>
 #include <stdint.h>
 
 void findPOI(Pixy2I2C* Cam);
-void move(Servo* ServoMotor);
+void setDesiredPOI(uint8_t num);
+
+void sendPosition(Adafruit_GPS* GPS, RH_RF95* RFM);
+void move(Adafruit_BNO055* BNO, Servo* ServoMotor, bool force=false);
